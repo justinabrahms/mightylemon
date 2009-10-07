56c2f412bc01a441dbd5e45e0db5970d82289751
Justin Lilly <justin@justinlilly.com>
2009-10-07 00:52:45 -0700

    Cleaning up the API a bit

--- apps/api/handlers.py
+++ apps/api/handlers.py
@@ -4,17 +4,30 @@ from blog.models import Post
 
 class AnonBlogPostHandler(AnonymousBaseHandler):
     model = Post
-    fields = ('title', 'tags', 'pub_date')
+    fields = ('post_url','tags','title','blog_title','published_on','slug')
 
 class BlogPostHandler(BaseHandler):
     model = Post
     allowed_methods = ('GET',)
     exclude = ('id','enable_comments','markup_type','create_date', 'permalink', 'body')
+    fields = ('post_url','tags','title','blog_title','published_on','slug')
     anonymous = AnonBlogPostHandler
 
     def list(self, request):
         obj_list = Post.objects.filter(active=True)
         return obj_list
+
+    @classmethod
+    def published_on(self, post_obj):
+        return post_obj.pub_date
+
+    @classmethod
+    def blog_title(self, post_obj):
+        return post_obj.blog.title
+
+    @classmethod
+    def post_url(self, post_obj):
+        return post_obj.get_absolute_url()
 
     def read(self, request, **kwargs):
         post_slug = kwargs.get('post_slug', None)
