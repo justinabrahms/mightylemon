9e63c13fe3718e339254f0656ec516631c37a551
Justin Lilly <justin@justinlilly.com>
2009-11-28 19:05:15 -0500

    Changing some links around. This is a backwards incompatible change with the currently launched version of my site, so this code won't be pushed until the redesign is done.

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 
 from django.conf import settings
 from django.conf.urls.defaults import *
+from django.core.urlresolvers import reverse
 from django.contrib import admin
 
 admin.autodiscover()
@@ -11,18 +12,21 @@ handler500 = "mightylemon.views.server_error"
 urlpatterns = patterns("",
     url(r"^about/$", "django.views.generic.simple.direct_to_template", {
         "template": "about.html",
-    }),
+    }, name="about_me"),
     url(r"^hire-me/$", "django.views.generic.simple.direct_to_template", {
     "template": "hire-me.html",
     }),
+    url(r"^coding/", include('codingrepo.urls')),
     url(r"^admin/doc/", include("django.contrib.admindocs.urls")),
     url(r"^admin/(.*)", admin.site.root),
     url(r"^blog/", include("blog.urls")),
     url(r"^comments/", include("django.contrib.comments.urls")),
-    url(r"^links/", include("links.urls")),
-    url(r"^events/", include("events.urls")),
+    # url(r"^links/", include("links.urls")),
+    # url(r"^events/", include("events.urls")),
+    url(r"^work/", include("portfolio.urls")),
+    url(r"^speaking/", include("speaking.urls")),
     url(r"^api/", include("api.urls")),
-    url(r"^$", "blog.views.homepage", name="oebfare_home"),
+    url(r"^$", "django.views.generic.simple.redirect_to", {'url':'/blog/'}, name="oebfare_home"),
 )
 
 if settings.LOCAL_DEVELOPMENT:
