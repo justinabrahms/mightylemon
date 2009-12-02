14c23c4050d4460adc0f37e77be772bffd858ff3
Justin Lilly <justin@justinlilly.com>
2009-12-02 03:10:30 -0500

    Removing now unneeded admins.

--- mightylemon/apps/codingrepo/admin.py
+++ mightylemon/apps/codingrepo/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from helpers.admin import SlugFieldOnly
-from codingrepo.models import Language, License, CodeProject
+from codingrepo.models import CodeProject
 
-admin.site.register(Language, SlugFieldOnly)
-admin.site.register(License, SlugFieldOnly)
 admin.site.register(CodeProject, SlugFieldOnly)
