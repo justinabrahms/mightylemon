3f3a0f2eabd1625a0affc9ba8a490600afacefcd
Justin Lilly <justin@justinlilly.com>
2009-12-02 03:10:10 -0500

    Pulling archive_year into its own view for overriding make_object_list

--- mightylemon/apps/blog/views.py
+++ mightylemon/apps/blog/views.py
@@ -1,9 +1,10 @@
-
 from django.views.generic import date_based
 from django.views.generic import list_detail
+from django.views.generic.simple import redirect_to
 from blog.models import Post
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
 
 def privileged_post_queryset(view_func):
     def _wrapped_view(request, **kwargs):
@@ -28,8 +29,20 @@ homepage = privileged_post_queryset(homepage)
 object_detail = privileged_post_queryset(date_based.object_detail)
 archive_day = privileged_post_queryset(date_based.archive_day)
 archive_month = privileged_post_queryset(date_based.archive_month)
-archive_year = privileged_post_queryset(date_based.archive_year)
 archive_index = privileged_post_queryset(date_based.archive_index)
+
+archive_year = privileged_post_queryset(date_based.archive_year)
+
+def archive_year(request, year, **kwargs):
+    defaults = {
+        'date_field': 'pub_date',
+        'make_object_list': True,
+        'queryset': request.blog.posts.active()
+    }
+    if request.user.is_authenticated():
+        defaults.update({'queryset':request.blog.posts.all()})
+    defaults.update(kwargs)
+    return date_based.archive_year(request, year, **defaults)
 
 def archive_full(request, **kwargs):
     return render_to_response("blog/post_archive_full.html", {
